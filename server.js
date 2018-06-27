26f3cbcf66c928a6dd0b5da22bccf29f6f88ac33
Jonah <younes.gh@chmail.ir>
2018-06-27 20:22:05 +0430

    const port is added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
